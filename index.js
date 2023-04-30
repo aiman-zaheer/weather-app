40cd4226864d2898bf18d15615575a7e937b97ff
aiman-zaheer <124160176+aiman-zaheer@users.noreply.github.com>
2023-05-01 00:34:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-
-const http = require('http');
-const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const requests = require('requests');
-const { json } = require('express');
-const { error } = require('console');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use('/home',express.static(__dirname)); // '/public' nh dyngy tw '/' is py bydefault files load horhi then
+app.use(bodyParser.urlencoded({extended:true}));
 const homeFile = fs.readFileSync("index.html","UTF-8");
+app.use('/',express.static(__dirname)); // '/public' nh dyngy tw '/' is py bydefault files load horhi then
+
 
 const realValues = (htmlData,newvalue)=>{
   let temperature = htmlData;
@@ -20,33 +17,44 @@ const realValues = (htmlData,newvalue)=>{
   temperature = temperature.replace("{%city%}",newvalue.name);
   temperature = temperature.replace("{%country%}",newvalue.sys.country);
   temperature = temperature.replace("{%weatherStatus%}",newvalue.weather[0].main);
-
   return temperature;
 }
-let url = 'https://api.openweathermap.org/data/2.5/weather?q=Karachi&appid=184b937fbff28f2227b26eb6d36757ae';
 
-app.get('/home/update',(req,res)=>{
-  
-    requests(url) 
+app.post('/',(req,res)=>{
+
+  console.log(req.body.cityname);
+  let city = req.body.cityname;
+  if(city !== ""){
+  let url2 = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=184b937fbff28f2227b26eb6d36757ae";
+
+  requests(url2) 
     .on('data',(chunk)=>{
       
       const objdata = JSON.parse(chunk); // convert JSON into Object
       const arrdata = [objdata]; // convert object into array
 
       let realData = arrdata.map(value => realValues(homeFile,value)).join("");
-      
+
       res.write(realData);
-    
+      
     })
     .on('end',(err)=>{
-      console.log(err+" error");
+      console.log(err);
       res.end();
     })
+    
+  }
+  else{
+    res.send("Please Enter City Name");
+  }
 })
+
+
 app.get('/*',(req,res)=>{
   res.send("File not found");
 })
 
-app.listen(5000,(req,res)=>{
-  console.log("working on port 5000");
+app.listen(7000,(req,res)=>{
+  console.log("working on port 7000");
+  
 })
